98dc81db731b03c5b13bbcad1f9636fbb56d3220
yun8boo <yuma051521@gmail.com>
2022-01-13 22:56:48 +0900

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/models/user.model';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+  async validateUser(
+    id: number,
+    password: string,
+  ): Promise<Omit<User, 'password'> | null> {
+    const user = await this.usersService.user(id);
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,5 +3,25 @@ import { User } from './models/user.model';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User = [];
+  private readonly users: User[] = [
+    {
+      id: 1,
+      email: 'asd@test.com',
+      password: '12345',
+    },
+    {
+      id: 2,
+      email: 'asdf@test.com',
+      password: '12345',
+    },
+    {
+      id: 3,
+      email: 'asdg@test.com',
+      password: '12345',
+    },
+  ];
+
+  async user(id: number): Promise<User | undefined> {
+    return this.users.find((user) => user.id === id);
+  }
 }
